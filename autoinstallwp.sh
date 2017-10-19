dca736a5f7786bdcd2174a0af4b3cd38a27c658d
about.giuseppedanna.com <gd@giuseppedanna.com>
2017-10-19 12:15:47 +0200

    Update autoinstallwp.sh

--- autoinstallwp.sh
+++ autoinstallwp.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -e
+
+# Auto Install Wordpress, version 1
+
+#  Performed by Ing. Giuseppe D'Anna
+#  -------
+#  This script uses quite a number of features that will be explained later on.
+
 clear
 echo "============================================"
-echo "WordPress Install Script"
+echo "WordPress Auto Install Script"
 echo "============================================"
 echo "Database Name: "
 read -e dbname
@@ -9,52 +16,64 @@ echo "Database User: "
 read -e dbuser
 echo "Database Password: "
 read -s dbpass
+echo "Language to install (ex: it, en, us):"
+read -s language
 echo "run install? (y/n)"
 read -e run
+
 if [ "$run" == n ] ; then
-exit
+    exit
 else
-echo "============================================"
-echo "A robot is now installing WordPress for you."
-echo "============================================"
-#download wordpress
-curl -O https://wordpress.org/latest.tar.gz
-#unzip wordpress
-tar -zxvf latest.tar.gz
-#change dir to wordpress
-cd wordpress
-#copy file to parent dir
-cp -rf . ..
-#move back to parent dir
-cd ..
-#remove files from wordpress folder
-rm -R wordpress
-#create wp config
-cp wp-config-sample.php wp-config.php
-#set database details with perl find and replace
-perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
-perl -pi -e "s/username_here/$dbuser/g" wp-config.php
-perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+    echo "============================================"
+    echo "A robot is now installing WordPress for you."
+    echo "============================================"
+    
+    # Download Wordpress
+    curl -O https://wordpress.org/latest.tar.gz
+    
+    # Unzip Wordpress
+    tar -zxvf latest.tar.gz
+    
+    # Change dir to wordpress
+    cd wordpress
+    
+    # Copy file to parent dir
+    cp -rf . ..
+    
+    # Move back to parent dir
+    cd ..
+    
+    # Remove files from wordpress folder
+    rm -R wordpress
+    
+    # Create wp config
+    cp wp-config-sample.php wp-config.php
+    
+    # Set database details with perl find and replace
+    perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+    perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+    perl -pi -e "s/password_here/$dbpass/g" wp-config.php
 
-#set WP salts
-perl -i -pe'
-  BEGIN {
-    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-  }
-  s/put your unique phrase here/salt()/ge
-' wp-config.php
+    #Set WP salts
+    perl -i -pe'
+      BEGIN {
+        @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+        push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+        sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+      }
+      s/put your unique phrase here/salt()/ge
+    ' wp-config.php
 
-#create uploads folder and set permissions
-mkdir wp-content/uploads
-chmod 775 wp-content/uploads
-echo "Cleaning..."
-#remove zip file
-rm latest.tar.gz
-#remove bash script
-rm wp.sh
-echo "========================="
-echo "Installation is complete."
-echo "========================="
+    #create uploads folder and set permissions
+    mkdir wp-content/uploads
+    chmod 775 wp-content/uploads
+    
+    echo "Cleaning..."
+    #remove zip file
+    rm latest.tar.gz
+    #remove bash script
+    rm autoinstallwp.sh
+    echo "========================="
+    echo "Installation is complete."
+    echo "========================="
 fi
